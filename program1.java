6c85e7f16c2e97c7de4bb33aa47fbe6c08eb7e96
Kateryna Borysova <katarina.bk@ukr.net>
2019-03-10 19:21:22 +0200

    commit2

--- program1.java
+++ program1.java
@@ -1,5 +1,7 @@
-public static void main(String[] args) {
+public class Main {
 
-        System.out.println("HELLO!");
-        }
-        }
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORD!");
+    }
+}
